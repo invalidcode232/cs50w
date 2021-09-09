28bcd60a353026317afab7627da8c54385297278
invalidcode <78639481+invalidcode232@users.noreply.github.com>
2021-09-09 20:43:34 +0700

    Finish tasks

--- lectures/lecture3/hello/urls.py
+++ lectures/lecture3/hello/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+app_name = 'Hello'
 urlpatterns = [
     path("", views.index, name="Index"),
     path("<str:name>", views.greet, name="greet"),
--- lectures/lecture3/tasks/urls.py
+++ lectures/lecture3/tasks/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name = 'tasks'
 urlpatterns = [
     path("", views.index, name="Index"),
     path("add", views.add, name="Add"),
