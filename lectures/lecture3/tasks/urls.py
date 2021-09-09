98f8207bf71bf1c102a87e2e290e71142c00d660
invalidcode <78639481+invalidcode232@users.noreply.github.com>
2021-09-09 18:10:25 +0700

    Added new years and greet

--- lectures/lecture3/tasks/urls.py
+++ lectures/lecture3/tasks/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="Index")
+    path("", views.index, name="Index"),
+    path("add", views.add, name="Add"),
 ]
