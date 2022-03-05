4f1543d46c33de7cb9014e2aa3fbeee875ae69e2
invalidcode <78639481+invalidcode232@users.noreply.github.com>
2022-03-05 18:39:26 +0700

    bruh

--- capstone/capstone/settings.py
+++ capstone/capstone/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'capstone.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'NAME_OF_DB',
+        'USER': 'DB_USER_NAME',
+        'PASSWORD': 'DB_PASSWORD',
+        'HOST': 'localhost',
+        'PORT': 'PORT_NUMBER',
     }
 }
 
